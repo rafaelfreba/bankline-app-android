2bc4f09770f2f5ca2e861006097e5f09af1a51bc
Rafael <rafaelfreba@gmail.com>
2022-05-05 17:25:38 -0400

    versao final do app android

---
+++ app/src/main/java/me/dio/bankline/data/BanklineRepository.kt
@@ -0,0 +1,31 @@
+package me.dio.bankline.data
+
+import android.util.Log
+import me.dio.bankline.data.remote.BanklineApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import androidx.lifecycle.liveData as liveData1
+
+object BanklineRepository {
+
+    private val TAG = javaClass.simpleName
+
+    private val restApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://10.15.4.70:8090/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(BanklineApi::class.java)
+    }
+
+        fun findBankStatement(accountHolderId: Int) = liveData1 {
+            emit(State.Wait)
+            try {
+                emit(State.Success(data = restApi.findBankStatement(accountHolderId)))
+            }catch(e: Exception){
+                Log.e(TAG, e.message, e)
+                emit(State.Error(e.message))
+            }
+        }
+
+}
